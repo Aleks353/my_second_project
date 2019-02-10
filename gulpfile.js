ed803903e98f2b4d376aea8de4d9e6d8c652d9c5
Alex <you@example.com>
2019-02-10 04:20:33 +0200

    new

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,24 @@ let cleanCSS = require('gulp-clean-css'); // Минификация css файл
 let includer = require('gulp-htmlincluder'); // Конкатинация файлов HTML (их склеивание)
 let connect = require('gulp-connect'); // Сервер
 let livereload = require('gulp-livereload'); // Livereload
+let replace = require('gulp-html-replace');
+let spritecreator = require('gulp.spritesmith');
+
+// Создание спрайтов
+gulp.task('sprite', async function(){
+	var spriteData = gulp.src('dev/img/icons/*.png')
+						.pipe(spritecreator({
+							imgName: 'sprite.png',
+							cssName: 'sprite.css',
+							algorithm: 'binary-tree'
+						}));
+	spriteData.img.pipe(gulp.dest('build/img/'));
+	spriteData.css.pipe(gulp.dest('build/css/'));
+});
 
 // Конкатинация файлов CSS (их склеивание)
 gulp.task('concat', async function(){
-	return gulp.src('dev/css/**/*.css')
+	return gulp.src(['dev/css/reset.css', 'dev/css/**/*.css'])
 	.pipe(concatCss("style.css"))
 	.pipe(cleanCSS({compatibility: 'ie8'})) // Минификация css файлов
 	.pipe(gulp.dest('build/css/'))
@@ -16,8 +30,11 @@ gulp.task('concat', async function(){
 
 // Конкатинация файлов HTML (их склеивание)
 gulp.task('html', async function() {
-   return gulp.src('dev/*.html')
+   return gulp.src('dev/**/*.html')
    .pipe(includer())
+   .pipe(replace({
+   	css: 'css/style.css'
+   }))
    .pipe(gulp.dest('build/'))
    .pipe(connect.reload());
 });
@@ -37,9 +54,7 @@ async function watch() {
 }
 
 // Комплексный запуск
-gulp.task('default', 
-	gulp.series('connect', 'html', 'concat', watch)
-);
+gulp.task('default', gulp.series('connect', 'html', 'concat', watch));
 
 
 	
